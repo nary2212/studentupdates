e3764b3093cd2ec412fd568c5d72598d21869d59
nary2212 <119007235+nary2212@users.noreply.github.com>
2022-11-25 00:01:53 +0100

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,8 @@
+
+<script>  
+	function fun() {  
+	var x = "hi";
+		document.getElementById("output").innerHTML = "hi";  
+		document.getElementById("output").innerHTML = "wrong details";
+	}  
+</script>
