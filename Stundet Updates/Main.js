d5076f99bb2d7e9c0b7214c0b4a6e23e2afce0c4
nary2212 <119007235+nary2212@users.noreply.github.com>
2022-11-27 23:31:45 +0100

    fi

--- Stundet Updates/Main.js
+++
@@ -1,8 +0,0 @@
-
-<script>  
-	function fun() {  
-	var x = "hi";
-		document.getElementById("output").innerHTML = "hi";  
-		document.getElementById("output").innerHTML = "wrong details";
-	}  
-</script>
